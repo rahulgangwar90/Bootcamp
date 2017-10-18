c81696c5695a9c3dee66824d82397fe6126565cb
rahul <rahul.babu@tothenew.com>
2017-10-18 13:29:17 +0530

    HBM: list records using createQuery

--- JavaApp/src/main/java/com/demo/hbm/HbmMain.java
+++ JavaApp/src/main/java/com/demo/hbm/HbmMain.java
@@ -4,21 +4,38 @@ import com.demo.hbm.common.HibernateUtil;
 import org.hibernate.Session;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by rahul on 17/10/17.
  */
 public class HbmMain {
+
+    static Session session = HibernateUtil.getSessionFactory().openSession();
+
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
+//        addUser();
+        listUsers();
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public static void addUser(){
         User user = new User();
         user.setUsername("rahul");
         user.setCreatedBy("system");
         user.setCreatedDate(new Date());
         session.save(user);
-        session.getTransaction().commit();
         System.out.println("Saved user: " + user);
-        session.close();
+    }
+
+    public static void listUsers(){
+        List users = session.createQuery("FROM User").list();
+        for (Iterator iterator = users.iterator(); iterator.hasNext();){
+            User user = (User) iterator.next();
+            System.out.println("Username: " + user.getUsername());
+        }
     }
 }
